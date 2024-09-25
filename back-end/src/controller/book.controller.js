5823d51bef2b448529805d245421deb77f568164
AshishChandra95 <ashishchandra961@gmail.com>
2024-09-25 11:21:52 +0530

    done update

--- back-end/src/controller/book.controller.js
+++ back-end/src/controller/book.controller.js
@@ -4,8 +4,10 @@ export const getBook = async(req, res) => {
     try {
         const book = await Book.find();
         res.status(200).json(book);
+        console.log("book is ready to show");
+        
     } catch (error) {
-        console.log("Error: ", error);
+        console.log("Books Page Error: ", error);
         res.status(500).json(error);
     }
 };
